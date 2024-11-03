6ea19320bd5b6c50576d392429e3b754a33c3ef2
Junsen Huang <97695812+WayfaringKid@users.noreply.github.com>
2024-11-03 16:32:41 -0500

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,9 @@
 /** @type {import('next').NextConfig} */
 
-// next.config.js
 const nextConfig = {
   eslint: {
     ignoreDuringBuilds: true, // 完全忽略 ESLint 错误
   },
 };
 
-module.exports = nextConfig;
+export default nextConfig;

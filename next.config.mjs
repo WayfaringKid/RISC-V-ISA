07d6343ca18d8021dd4a1cae9b620797fe0571ed
Junsen Huang <97695812+WayfaringKid@users.noreply.github.com>
2024-11-03 16:31:38 -0500

    disable eslint globally

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
 
-export default nextConfig;
+// next.config.js
+const nextConfig = {
+  eslint: {
+    ignoreDuringBuilds: true, // 完全忽略 ESLint 错误
+  },
+};
+
+module.exports = nextConfig;
